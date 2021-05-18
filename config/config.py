af993b9bc4bbc87279e845c14d9b375cfecd9ec4
Mikhail Kashrskii <sofrony@yandex-team.ru>
2021-05-18 12:20:35 +0300

    use t.me/first_personal_assistant_bot by default

--- config/config.py
+++ config/config.py
@@ -1,3 +1,3 @@
-TOKEN = '1660376392:AAHqeib5RhnCGZPUl8p-c4Fi8Cc5iB381PM'
+TOKEN = '1778849675:AAGj5pNWCLHq8qvISuXlV9iTub1jkm36j-g'
 MODEL_PATH = 'https://storage.yandexcloud.net/assistant-bot-bucket/saved_models/my_model_1.hdf5'
 DATA_PATH = 'data'
